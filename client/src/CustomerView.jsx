3c06f8add4f7d2693e5a0d5cf8392a7b12feec8e
Poisen0 <niklasmeyer7777@gmail.com>
2023-01-15 21:50:18 +0100

    changes to add function

--- client/src/CustomerView.jsx
+++ client/src/CustomerView.jsx
@@ -1,17 +1,29 @@
 import React from "react";
-import AddCustomer from "./AddCustomer";
+//import AddCustomer from "./AddCustomer";
 import { ButtonToolbar } from "react-bootstrap";
 
-import { Button, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import { Button, Table, TableBody, TableCell, TableHead, TableRow, Modal, Box } from "@mui/material";
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
 class Customer extends React.Component{
-
+    
     constructor(props){
         super(props); 
             this.state = {
                 list: [], 
-                showAddCustomer : false 
+                showAddCustomer : false,
+                modalOpen: false
             };
     }
     componentDidMount(){
@@ -28,8 +40,6 @@ class Customer extends React.Component{
         console.log("add clicked");
     }
     
-
-
     handleDelete = (id) => {
         console.log(id)
         fetch(process.env.REACT_APP_API_BASE + "/company/remove/"+id, {method: 'DELETE'})
@@ -40,13 +50,91 @@ class Customer extends React.Component{
             .then (() => this.componentDidMount())
     }
 
+    handleModalClose = () => {
+        this.setState({modalOpen:false});
+    }
+    handleModalOpen = () =>{
+        this.setState({modalOpen:true});
+    }
+    handleModalSubmit = (e) =>{
+        e.preventDefault()
+        let payload={
+            "companyName": e.target.customerName.value,
+            "department": e.target.department.value,
+            "address": {
+                "country": e.target.country.value,
+                "area": e.target.area.value,
+                "city": e.target.city.value,
+                "zipCode": e.target.zipCode.value,
+                "streetName": e.target.streetName.value,
+                "houseNumber": e.target.houseNumber.value
+            }
+        }
+        fetch(process.env.REACT_APP_API_BASE + "/company/create", {method: 'POST', headers:{'Content-Type':"application/json"},body: JSON.stringify(payload)})
+            .then( response => { 
+                console.log(response) 
+            })
+            .then (() => this.handleModalClose())
+            .then (() => this.componentDidMount()) 
+    }
+
     render() {
-        let disableAddCustomer = () => this.setState({showAddCustomer:false}); 
 
         return(    
             <div className="view">
             <h1>Customers</h1>
-            <AddCustomer show={this.state.showAddCustomer} onHide = {disableAddCustomer}/>
+            <Button variant="contained" onClick={this.handleModalOpen}>+ Add Customer</Button>
+            <Modal  open={this.state.modalOpen} onClose={this.handleModalClose}
+                >
+                <Box sx={style}>
+                <div>
+                    <form onSubmit={this.handleModalSubmit}>
+                        <div id="Div_CustomerName">
+                            <label>
+                                Customer Name:
+                                <input type="text" name="customerName" />
+                            </label>
+                        </div>
+                        <div id="Div_Department">
+                            <label>
+                                Department:
+                                <input type="text" name="department" />
+                            </label>
+                        </div>
+                        <div id="Div_Adress">
+                            <h3>Adress:</h3>
+                            <label>
+                                Country:
+                                <input type="text" name="country" />
+                            </label>
+                            <label>
+                                Area:
+                                <input type="text" name="area" />
+                            </label>
+                            <label>
+                                City:
+                                <input type="text" name="city" />
+                            </label>
+                            <label>
+                                Zip Code:
+                                <input type="text" name="zipCode" />
+                            </label>
+                            <label>
+                                Street Name:
+                                <input type="text" name="streetName" />
+                            </label>
+                            <label>
+                                House Number:
+                                <input type="text" name="houseNumber" />
+                            </label>
+                        </div>
+                        <div className="Button_Submit">
+                            <Button variant="contained" type="submit" value="submit">Submit</Button>
+                        </div>
+                    </form>
+                </div>
+                </Box>
+            </Modal>
             <Table>
                 <TableHead>
                     <TableRow>
