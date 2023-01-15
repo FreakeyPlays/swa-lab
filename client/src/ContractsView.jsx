c324e5a9b75cb06488428597480651b718949de8
Glikoit00 <glikoit00@hs-esslingen.de>
2023-01-16 00:31:37 +0100

    add contractrs and user popups

--- client/src/ContractsView.jsx
+++ client/src/ContractsView.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 
-import { Button, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import { Button, Table, TableBody, TableCell, TableHead, TableRow, Modal, Box, Typography } from "@mui/material";
 
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
 
-class Customer extends React.Component{
+class Contracts extends React.Component{
 
     constructor(props){
         super(props); 
             this.state = {
-                contracts: []
+                contracts: [], 
+                showAddCustomer : false,
+                modalOpen: false
             }; 
     }
     componentDidMount(){
@@ -29,6 +42,48 @@ class Customer extends React.Component{
             .then (() => this.setState({status: 'Delete successful'}))
             .then (() => this.componentDidMount())
     }
+    handleModalClose = () => {
+        this.setState({modalOpen:false});
+    }
+    handleModalOpen = () =>{
+        this.setState({modalOpen:true});
+    }
+    handleModalSubmit = (e) =>{
+        e.preventDefault()
+        let payload={
+            "licenseKey": e.target.licenseKey.value,
+            "startDate": e.target.startDate.value,
+            "endDate": e.target.endDate.value,
+            "version": e.target.version.value,
+            "ips": [
+                {
+                    "address":e.target..value
+                },
+                {
+                    "address":e.target..value
+                }
+            ],
+            "users": [
+                {
+                    "id": e.target.version.value
+                }
+            ],
+            "features": [
+                {
+                    "number": e.target.version.value
+                },
+                {
+                    "number": e.target.version.value
+                }
+            ]
+        }
+        fetch(process.env.REACT_APP_API_BASE + "/contract/create", {method: 'POST', headers:{'Content-Type':"application/json"},body: JSON.stringify(payload)})
+            .then( response => { 
+                console.log(response) 
+            })
+            .then (() => this.handleModalClose())
+            .then (() => this.componentDidMount()) 
+    }
 
     render() {
 
@@ -36,7 +91,36 @@ class Customer extends React.Component{
             
             <div className="view">
             <h1>Contracts</h1>
-            <Button id="addbtn_contracts" variant="contained" size="medium">Add</Button>
+            <Button id="addbtn_contracts" variant="contained" size="medium" onClick={this.handleModalOpen}>+ Add Contract</Button>
+            <Modal
+                open={this.state.modalOpen} onClose={this.handleModalClose}
+                aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+                
+            <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+                Add contract details
+            </Typography>
+                <div>
+                    <form onSubmit={this.handleModalSubmit}>
+                        <div >         
+                            <input type="text" className="input_add" placeholder="Start date" name="startDate" />
+                            <input type="text" className="input_add" placeholder="End date" name="endDate" />
+                        </div> 
+                        <div className="address">  
+                        <input type="text" id="str" className="input_add" placeholder="Key" name="licenseKey" />
+                        <input type="text" id="nr" className="input_add" placeholder="version" name="version" />  
+                        </div> 
+                        <div className="address">
+                            <input type="text" id="nr" className="input_add" placeholder="CompanyID" name="companyId" />
+                            <input type="text" id="str" className="input_add" placeholder="ISP" name="address" />
+                        </div>                                        
+                        <div className="Button_Submit">
+                            <Button variant="contained" type="submit" value="submit">Submit</Button>
+                        </div>
+                    </form>            
+                </div>
+            </Box>
+                </Modal>
             <Table>
                 <TableHead>
                     <TableRow>
@@ -57,7 +141,8 @@ class Customer extends React.Component{
                             <TableCell>{contract.endDate}</TableCell>
                             <TableCell>{contract.version}</TableCell>
                             <TableCell><Button variant="contained" size="small" onClick={this.handleEdit}>Edit</Button></TableCell>
-                            <TableCell><Button variant="contained" size="small" onClick={() => {this.handleDelete(contract.id)}}>Delete</Button></TableCell>                            <TableCell><Button variant="contained" size="small" onClick={this.handleShowContracts}>Details</Button></TableCell>
+                            <TableCell><Button variant="contained" size="small" onClick={() => {this.handleDelete(contract.id)}}>Delete</Button></TableCell>
+                            <TableCell><Button variant="contained" size="small" onClick={this.handleShowContracts}>Details</Button></TableCell>
                         </TableRow>
                     ))}
                     </TableBody>
@@ -68,4 +153,4 @@ class Customer extends React.Component{
 }
 
 
-export default Customer; 
+export default Contracts; 
